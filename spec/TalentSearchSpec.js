6b3de4f9d79e56553bf50c086cf6428732a5ead2
Louis Kirkham <chew@talk21.com>
2021-01-22 13:44:26 +0000

    first passing test

---
+++ spec/TalentSearchSpec.js
@@ -0,0 +1,36 @@
+describe("TalentSearch", () => {
+
+  let search
+  const example = [
+    {
+      "name": "Homer Simpson",
+      "location": "Springfield",
+      "date_of_birth": "1956-05-12"
+    },
+    {
+      "name": "Frank Reynolds",
+      "location": "Philidelphia",
+      "date_of_birth": "1944-11-17"
+    },
+    {
+      "name": "Diane Nguyen",
+      "location": "Los Angeles",
+      "date_of_birth": "1980-03-19"
+    },
+    {
+      "name": "Krusty the Clown",
+      "location": "SpringField",
+      "date_of_birth": "1957-10-29"
+    }
+  ]
+
+  beforeEach(function() {
+    search = new TalentSearch();
+  });
+
+  describe("byLocation", () => {
+    it("returns an array of a single matching name", () => {
+      expect(search.byLocation(example, "Philidelphia")).toEqual(["Frank Reynolds"]);
+    });
+  });
+});
---
+++ src/TalentSearch.js
@@ -0,0 +1,6 @@
+class TalentSearch {
+
+  byLocation(array, location) {
+    return ["Frank Reynolds"]
+  }
+}
