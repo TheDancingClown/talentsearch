cced4695d6f702174189df31f0c4c66d7f44294a
Louis Kirkham <chew@talk21.com>
2021-01-22 14:00:20 +0000

    removed exmaple jasmine files

--- src/TalentSearch.js
+++ src/TalentSearch.js
@@ -1,8 +1,8 @@
 class TalentSearch {
 
   byLocation(data, location) {
-    let matches = data.filter(talent => talent.location.toLowerCase() === location.toLowerCase())
-    let matchedNames = matches.map(talent => talent.name)
-    return matchedNames
-  }
-}
+    let matches = data.filter(talent => talent.location.toLowerCase() === location.toLowerCase());
+    let matchedNames = matches.map(talent => talent.name);
+    return matchedNames;
+  };
+};
