b5b540d4fc67dfeacaf07ef841c38ef67fab5525
Louis Kirkham <chew@talk21.com>
2021-01-23 21:03:26 +0000

    using XMLHttpRequest

--- src/interface.js
+++ src/interface.js
@@ -3,25 +3,32 @@ searchButtonPressed();
 
 function searchForTalent(talentPool, location) {
   if(talentPool==='North America') {
-    var request = '../example.json'
-  } //conditional in case other talent pools needed
-  fetch(request)
-  .then(response => response.json())
-  .then(data => {
-    let matchedNames = search.byLocation(data, location);
-    let rows = ''
-    for(let i = 0; i < matchedNames.length; i++) {
-      rows += `<p>${matchedNames[i]}</p>`;
-    }
-    document.getElementById('talent-display').innerHTML = rows ? rows : "<p>No matches found</p>"
-  });
+    var requestURL = './example.json'
+  }
+  let request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    const data = request.response;
+    showTalent(data, location);
+  };
 };
+
+function showTalent(obj, location) {
+  let matchedNames = search.byLocation(obj, location);
+  let rows = '';
+  for(let i = 0; i < matchedNames.length; i++) {
+    rows += `<p>${matchedNames[i]}</p>`;
+  };
+  document.getElementById('talent-display').innerHTML = rows ? rows : "<p>No matches found</p>";
+}
 
 function searchButtonPressed() {
   document.getElementById("search-button").addEventListener("click", function(clickEvent) {
     clickEvent.preventDefault();
-    let talentPool = document.getElementById("talent-selector").value
-    let location = document.getElementById("location-input").value
+    let talentPool = document.getElementById("talent-selector").value;
+    let location = document.getElementById("location-input").value;
     searchForTalent(talentPool, location);
   })
 };
--- testcafe/search_test.js
+++ testcafe/search_test.js
@@ -1,31 +1,31 @@
 import { Selector, RequestMock } from 'testcafe';
 
-const mock = RequestMock()
-  .onRequest('../example.json')
-  .respond([{
-    "name": "Homer Simpson",
-    "location": "Springfield",
-    "date_of_birth": "1956-05-12"
-  },
-  {
-    "name": "Frank Reynolds",
-    "location": "Philidelphia",
-    "date_of_birth": "1944-11-17"
-  },
-  {
-    "name": "Diane Nguyen",
-    "location": "Los Angeles",
-    "date_of_birth": "1980-03-19"
-  },
-  {
-    "name": "Krusty the Clown",
-    "location": "SpringField",
-		"date_of_birth": "1957-10-29"
-  }])
+// const mock = RequestMock()
+//   .onRequest('../example.json')
+//   .respond([{
+//     "name": "Homer Simpson",
+//     "location": "Springfield",
+//     "date_of_birth": "1956-05-12"
+//   },
+//   {
+//     "name": "Frank Reynolds",
+//     "location": "Philidelphia",
+//     "date_of_birth": "1944-11-17"
+//   },
+//   {
+//     "name": "Diane Nguyen",
+//     "location": "Los Angeles",
+//     "date_of_birth": "1980-03-19"
+//   },
+//   {
+//     "name": "Krusty the Clown",
+//     "location": "SpringField",
+// 		"date_of_birth": "1957-10-29"
+//   }])
 
 fixture(`Searching for talent`)
-  .page(`../TalentSearch.html`)
-  .requestHooks(mock);
+  .page(`../index.html`)
+  // .requestHooks(mock);
 
 test('Message displayed when no matches found', async t => {
     await t
