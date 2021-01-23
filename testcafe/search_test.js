9a848ee2d08fefd80a99b5b47eeaf4ef37003163
Louis Kirkham <chew@talk21.com>
2021-01-23 21:56:47 +0000

    testcafe mock passing

--- testcafe/search_test.js
+++ testcafe/search_test.js
@@ -1,31 +1,32 @@
 import { Selector, RequestMock } from 'testcafe';
-
-// const mock = RequestMock()
-//   .onRequest('../example.json')
-//   .respond([{
-//     "name": "Homer Simpson",
-//     "location": "Springfield",
-//     "date_of_birth": "1956-05-12"
-//   },
-//   {
-//     "name": "Frank Reynolds",
-//     "location": "Philidelphia",
-//     "date_of_birth": "1944-11-17"
-//   },
-//   {
-//     "name": "Diane Nguyen",
-//     "location": "Los Angeles",
-//     "date_of_birth": "1980-03-19"
-//   },
-//   {
-//     "name": "Krusty the Clown",
-//     "location": "SpringField",
-// 		"date_of_birth": "1957-10-29"
-//   }])
+const url = new RegExp('example.json')
+const mockedResponse = [{
+  "name": "Homer Simpson",
+  "location": "Springfield",
+  "date_of_birth": "1956-05-12"
+},
+{
+  "name": "Frank Reynolds",
+  "location": "Philidelphia",
+  "date_of_birth": "1944-11-17"
+},
+{
+  "name": "Diane Nguyen",
+  "location": "Los Angeles",
+  "date_of_birth": "1980-03-19"
+},
+{
+  "name": "Krusty the Clown",
+  "location": "SpringField",
+  "date_of_birth": "1957-10-29"
+}]
+const mock = RequestMock()
+  .onRequestTo(url)
+  .respond(mockedResponse, 200, {'Access-Control-Allow-Origin': '*'});
 
 fixture(`Searching for talent`)
   .page(`../index.html`)
-  // .requestHooks(mock);
+  .requestHooks(mock);
 
 test('Message displayed when no matches found', async t => {
     await t
@@ -33,11 +34,9 @@ test('Message displayed when no matches found', async t => {
       .click('#talent-selector')
       .click(Selector('option', { text: 'North America' }))
       // enter search parameter
-      .typeText('#location-input', 'Los Angeles')
+      .typeText('#location-input', 'New York')
       // click button
       .click('#search-button')
       // see results
-      .expect(Selector('#talent-display').innerHTML).eql('No matches found');
+      .expect(Selector('#talent-display').innerText).eql('No matches found');
 });
-
-//TypeError: event.setMock is not a function
